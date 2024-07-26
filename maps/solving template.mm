e460fd1317639a5e779a2fe64651681dabf8413d
AakashSJ <aakash.jadhav@nagarro.com>
2024-07-26 12:21:01 +0530

    coomm

--- maps/solving template.mm
+++ maps/solving template.mm
@@ -1,20 +1,7 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1721907264880" ID="ID_1863779422" MODIFIED="1721970011388" TEXT="yaar">
-<node CREATED="1721970012782" ID="ID_1198064115" MODIFIED="1721970031531" POSITION="right" TEXT="yaar bc seriously kai kai kaam aahet aani motivation kuthun yenaar aahe"/>
-<node CREATED="1721970031942" ID="ID_700395101" MODIFIED="1721970057427" POSITION="right">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      maybe motivation chi garaj nahi aahe ani kaam ach motivating enough aahe <b>doob ke dekho</b>
-    </p>
-  </body>
-</html>
-</richcontent>
-</node>
-<node CREATED="1721970059420" ID="ID_896561838" MODIFIED="1721970072580" POSITION="right" TEXT="okay pan kai yaar kai artha nahi aahe kashya lach "/>
+<node CREATED="1721972775645" ID="ID_1614688227" MODIFIED="1721972785100" POSITION="right" TEXT="kaama kai aahet bc"/>
+<node CREATED="1721972790622" ID="ID_387895278" MODIFIED="1721972801565" POSITION="right" TEXT="aata header la karaicha aahe okay"/>
 </node>
 </map>
