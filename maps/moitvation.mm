4de560370b65c35906ba13cd98877c700ac14404
AakashSJ <aakash.jadhav@nagarro.com>
2024-07-26 11:03:24 +0530

    commit mind maps

--- maps/moitvation.mm
+++ maps/moitvation.mm
@@ -1,11 +1,6 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1721714075529" ID="ID_1766694649" MODIFIED="1721714093066" TEXT="motivation">
-<node CREATED="1721748688611" ID="ID_1518011711" MODIFIED="1721748700720" POSITION="right" TEXT="okay bhechod chal bol aata kai pahije te ..."/>
-<node CREATED="1721748751537" ID="ID_959738797" MODIFIED="1721748758774" POSITION="right" TEXT="je pahije aasel te bol aata"/>
-<node CREATED="1721748759543" ID="ID_174192144" MODIFIED="1721748766030" POSITION="right" TEXT="kaam ma cha bolaicha kaa ?"/>
-<node CREATED="1721748767708" ID="ID_331868339" MODIFIED="1721748775044" POSITION="right" TEXT="chal ek toodo tracker banavu okay"/>
-<node CREATED="1721748775991" ID="ID_572707885" MODIFIED="1721748775991" POSITION="right" TEXT=""/>
 <node CREATED="1721748776741" ID="ID_1678449015" MODIFIED="1721748779534" POSITION="right" TEXT="todo">
 <node CREATED="1721748780623" ID="ID_1238944928" MODIFIED="1721748787445" TEXT="todo kai padla aahe ?">
 <node CREATED="1721748790041" ID="ID_1681469068" MODIFIED="1721748807230" TEXT="audible setup ?">
@@ -26,8 +21,7 @@
       ani print out madhe mark karaicha ? okay pan jar samjha mala freemind madhech show karaicha asel tar ????
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1721748991377" ID="ID_212232522" MODIFIED="1721748996104">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -44,8 +38,7 @@
       <b>level 1 la width max deichi</b>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
@@ -62,10 +55,76 @@
       job abroad
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
+<node CREATED="1721799807858" ID="ID_799302115" MODIFIED="1721799811441" TEXT="laptop heat sink">
+<node CREATED="1721799814455" ID="ID_1678837728" MODIFIED="1721799838514">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Main motivation</b>&#160;hi aahe ki rainy season madhe pc kai masta chalu aahe&#160;&#160;
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1721799840694" ID="ID_1396484633" MODIFIED="1721799846495" TEXT="patient portal "/>
+<node CREATED="1721799846826" ID="ID_593448129" MODIFIED="1721799848541" TEXT="docker "/>
+<node CREATED="1721799849010" ID="ID_793914810" MODIFIED="1721799850518" TEXT="postman"/>
+</node>
+<node CREATED="1721799861779" ID="ID_1253390185" MODIFIED="1721799871625" TEXT="ani simple fix aahe open and close case ahe"/>
+<node CREATED="1721799881973" ID="ID_878361367" MODIFIED="1721799896019" TEXT="amazon warit product pan aahe ... 389 waala with applicator"/>
+</node>
+</node>
+<node CREATED="1721796116864" ID="ID_761234088" MODIFIED="1721796123206" POSITION="right" TEXT="alarm clock">
+<node CREATED="1721796971548" ID="ID_253896028" MODIFIED="1721796976142" TEXT="time">
+<node CREATED="1721799674982" ID="ID_1481889958" MODIFIED="1721799677578" TEXT="..">
+<node CREATED="1721799678531" ID="ID_995838548" MODIFIED="1721799683399" TEXT="work">
+<node CREATED="1721799684916" ID="ID_742562351" MODIFIED="1721799708632" TEXT="feelings"/>
+</node>
+</node>
+</node>
+<node CREATED="1721796976725" ID="ID_1420271497" MODIFIED="1721796983806" TEXT="10 to 11">
+<node CREATED="1721796985104" ID="ID_1308527357" MODIFIED="1721796989388" TEXT="30min">
+<node CREATED="1721797007113" ID="ID_576119545" MODIFIED="1721797017410" TEXT="figuring out why user api is not working ??"/>
+</node>
+<node CREATED="1721796989758" ID="ID_1664622927" MODIFIED="1721796992858" TEXT="30mins">
+<node CREATED="1721798115803" ID="ID_1329768758" MODIFIED="1721798125710" TEXT="takun dila group warti unable to test further"/>
+</node>
+</node>
+<node CREATED="1721796976725" ID="ID_173542037" MODIFIED="1721799750406" TEXT="11 to 12">
+<node CREATED="1721796985104" ID="ID_63047042" MODIFIED="1721796989388" TEXT="30min">
+<node CREATED="1721799663631" ID="ID_741021540" MODIFIED="1721799669144" TEXT="poop - tea">
+<node CREATED="1721799695123" ID="ID_1647921874" MODIFIED="1721799696332" TEXT="tea"/>
+<node CREATED="1721799712498" ID="ID_1906732339" MODIFIED="1721799735282" TEXT="poop - bhenchod parat bota ghalu haga"/>
+<node CREATED="1721799736351" ID="ID_1847070465" MODIFIED="1721799743907" TEXT="laaj ach sodhli aahe mi tar"/>
+</node>
+</node>
+<node CREATED="1721796989758" ID="ID_501518538" MODIFIED="1721796992858" TEXT="30mins"/>
+</node>
+<node CREATED="1721796976725" ID="ID_1460644753" MODIFIED="1721799752753" TEXT="12 to 1">
+<node CREATED="1721796985104" ID="ID_1635925053" MODIFIED="1721796989388" TEXT="30min"/>
+<node CREATED="1721796989758" ID="ID_639721776" MODIFIED="1721796992858" TEXT="30mins"/>
+</node>
+<node CREATED="1721799753133" ID="ID_971906757" MODIFIED="1721799758141" TEXT="1 to 2"/>
+<node CREATED="1721796976725" ID="ID_1593283301" MODIFIED="1721799762323" TEXT="2 to 3">
+<node CREATED="1721796985104" ID="ID_1080963472" MODIFIED="1721796989388" TEXT="30min"/>
+<node CREATED="1721796989758" ID="ID_1518026221" MODIFIED="1721796992858" TEXT="30mins"/>
+</node>
+</node>
+<node CREATED="1721799051144" ID="ID_1432431827" MODIFIED="1721800497666" POSITION="right" TEXT="tasks ..">
+<node CREATED="1721800498592" ID="ID_606630634" MODIFIED="1721800522457" TEXT="unable to test further karan user information ach yet nahi aahe ani changes deplyed aahe ki nahi tech samjat nahi aahe"/>
+</node>
+<node CREATED="1721823666635" ID="ID_386665610" MODIFIED="1721823677302" POSITION="right" TEXT="kaama kai kai aahet bc "/>
+<node CREATED="1721823679870" ID="ID_1964923066" MODIFIED="1721823691363" POSITION="right" TEXT="paheli goshta bah toh kai bolala aahe ki"/>
+<node CREATED="1721823691760" ID="ID_296818790" MODIFIED="1721823696691" POSITION="right" TEXT="cn test ">
+<node CREATED="1721823701615" ID="ID_1654116175" MODIFIED="1721823713274" TEXT="technician pr merge for email change"/>
+</node>
+<node CREATED="1721823726120" ID="ID_108640469" MODIFIED="1721823735908" POSITION="right" TEXT="aur fhir ? "/>
+<node CREATED="1721823736836" ID="ID_1516848715" MODIFIED="1721823759884" POSITION="right" TEXT="feeling survey ">
+<node CREATED="1721823760878" ID="ID_914042498" MODIFIED="1721823760878" TEXT=""/>
 </node>
 </node>
 </map>
---
+++ maps/problem solving templeate.mm
@@ -0,0 +1,9 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1721800425194" ID="ID_551653993" MODIFIED="1721800432708" TEXT="problem solving templeate">
+<node CREATED="1721800437604" ID="ID_1137520932" MODIFIED="1721800455222" POSITION="right" TEXT="problem haan aahe ki aksh la ka kon kai bolat nahi aahe ??">
+<node CREATED="1721800456317" ID="ID_988038773" MODIFIED="1721800465652" TEXT="mi group warti message taakla aahe"/>
+<node CREATED="1721800465982" ID="ID_1281834378" MODIFIED="1721800479106" TEXT="maybe te ekka weglya call madhe astil already"/>
+</node>
+</node>
+</map>
