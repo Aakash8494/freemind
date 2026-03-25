ee0b80319143b086e1e554a4928e9c10ec610cca
Aakash Jadhav <72457949+Aakash8494@users.noreply.github.com>
2026-03-25 11:57:37 +0530

    final

---
+++ maps/test.mm
@@ -0,0 +1,50 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1773925783580" ID="ID_1848053933" MODIFIED="1773927012391" TEXT="distraction depo">
+<node CREATED="1773925954269" ID="ID_1775936686" MODIFIED="1773927022660" POSITION="right" TEXT="okay tar scene kai aahe"/>
+<node CREATED="1773927023648" ID="ID_15322975" MODIFIED="1773927028929" POSITION="right" TEXT="sanak jaate manje kai ?"/>
+<node CREATED="1773927029695" ID="ID_300732765" MODIFIED="1773927038225" POSITION="right" TEXT="manje mala jamat nahi tar mi ego war gheto ">
+<node CREATED="1773927068923" ID="ID_1091921190" MODIFIED="1773927075084" TEXT="psychological ?">
+<node CREATED="1773927089412" ID="ID_1825715647" MODIFIED="1773927184912">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        ego warti ghene manje ?
+
+        <ul>
+          <li>
+            manje ki ... ego warti ghene
+
+            <ul>
+              <li>
+                mi tar hero aahe tar mala ka nahi jamat aahe ... ?
+
+                <ul>
+                  <li>
+                    best solution ... dont take on ego be open mind
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1773927038816" ID="ID_1325401954" MODIFIED="1773927050006" POSITION="right" TEXT="kivah ek physical limitation aahe bc">
+<node CREATED="1773927079180" ID="ID_1716326455" MODIFIED="1773927082051" TEXT="physical ?">
+<node CREATED="1773927082051" ID="ID_1271762749" MODIFIED="1773927088136" TEXT="tea wagere mule ... ok"/>
+</node>
+</node>
+<node CREATED="1773927050433" ID="ID_67536846" MODIFIED="1773927052126" POSITION="right" TEXT="ok"/>
+</node>
+</map>
